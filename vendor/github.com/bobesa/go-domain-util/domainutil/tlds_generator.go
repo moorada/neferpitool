1749c1205b8ec2ad533c4c5f0b6345a6563d85a6
Giuseppe Trotta <704560+ohpe@users.noreply.github.com>
2023-02-21 14:43:35 +0100

    Repo cleanup

--- vendor/github.com/bobesa/go-domain-util/domainutil/tlds_generator.go
+++
@@ -1,3 +0,0 @@
-package domainutil
-
-//go:generate go run github.com/bobesa/go-domain-util/cmd/domainparser
--- vendor/github.com/moorada/whois/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/moorada/whois
-
-go 1.12
-
-require github.com/evilsocket/islazy v1.10.4
